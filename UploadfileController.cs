4c024bad7be5b952d3e056132c18fc417b698c53
Md.Farid Uddin Kiron <kironiitdu@gmail.com>
2022-06-06 20:48:12 +0800

    Create UploadfileController.cs

---
+++ UploadfileController.cs
@@ -0,0 +1,26 @@
+    [Route("api/Uploadfile")]
+    [ApiController]
+    public class UploadfileController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult Post([FromForm] FileModel file)
+        {
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", file.FileName);
+
+                using (Stream stream = new FileStream(path, FileMode.Create))
+                {
+                    file.FormFile.CopyTo(stream);
+                }
+
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+    }
